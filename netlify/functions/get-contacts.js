20e005941e0e794e12a30fc948c99d14c624c336
Maor Ganon <maorganon@MacBookPro.lan>
2026-01-18 08:54:04 +0200

    fix: add Neon serverless to external modules and CORS headers

--- netlify/functions/get-contacts.js
+++ netlify/functions/get-contacts.js
@@ -1,10 +1,26 @@
 const { neon } = require('@neondatabase/serverless');
 
 exports.handler = async (event) => {
+  // Handle CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
   // Only allow GET requests
   if (event.httpMethod !== 'GET') {
     return {
       statusCode: 405,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
       body: JSON.stringify({ error: 'Method not allowed' })
     };
   }
